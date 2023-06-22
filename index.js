1833895da1c006bc7b84b48c37566ef7f71b3454
Timo Bühlmann <timo.buehlmann@gmail.com>
2023-06-22 19:01:52 +0200

    implement optional caching (for development)

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 import * as dotenv from 'dotenv';
 import RequestManager from "./src/RequestManager.js";
+import fs from "fs";
 dotenv.config();
 
-const requestManager = new RequestManager()
+const cachePath = './cache.json'
+let projectsWithTasks = []
 
-const projects = await requestManager.request('/open/v1/project')
+if (process.argv.includes('--use-cache') && fs.existsSync(cachePath)) {
+  projectsWithTasks = JSON.parse(fs.readFileSync(cachePath))
+} else {
+  const requestManager = new RequestManager()
+  const projects = await requestManager.request('/open/v1/project')
 
-const projectsWithTasks = []
-for (const p of projects) {
-  console.log(`load project data for: ${p.name}`)
-  projectsWithTasks.push(await requestManager.request(`/open/v1/project/${p.id}/data`))
+  for (const p of projects) {
+    console.log(`load project data for: ${p.name}`)
+    projectsWithTasks.push(await requestManager.request(`/open/v1/project/${p.id}/data`))
+  }
+  fs.writeFileSync('./data.json', JSON.stringify(projectsWithTasks))
 }
 
 console.log(projectsWithTasks)
 
-// try to solve the problem with the query limit
+// use cache with: node index.js --use-cache
 // store everything in csv
-
-console.log(projectsWithTasks.filter((p) => p.hasOwnProperty('project')).length)
