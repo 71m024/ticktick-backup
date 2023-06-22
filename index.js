c8ff8bd19bbbca57870268df3bb19675848b318b
Timo Bühlmann <timo.buehlmann@gmail.com>
2023-06-22 18:11:33 +0200

    implement query-delay handling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as dotenv from 'dotenv'
-import getAccessToken from './src/getAccessToken.js'
+import * as dotenv from 'dotenv';
+import getAccessToken from './src/getAccessToken.js';
 import request from "./src/request.js";
-dotenv.config()
+dotenv.config();
 
 const accessToken = await getAccessToken()
 
@@ -14,18 +14,32 @@ const projects = await request('/open/v1/project', {
   redirect: 'follow'
 })
 
+const loadProject = async (p) => {
+  console.log(`load project data for: ${p.name}`)
+  const response = await request(`/open/v1/project/${p.id}/data`, {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  })
+  if (response.hasOwnProperty('project')) {
+    return response
+  } else if (response.hasOwnProperty('errorCode') && response.errorCode === 'exceed_query_limit') {
+    console.log('waiting because of query limit')
+    await new Promise(resolve => setTimeout(resolve, 10000))
+    return loadProject(p)
+  } else {
+    throw new Error(`could not load project: ${JSON.stringify(response)}`)
+  }
+}
+
 const projectsWithTasks = []
 for (const p of projects) {
-  console.log(`load project data for: ${p.name}`)
-  projectsWithTasks.push(
-    await request(`/open/v1/project/${p.id}/data`, {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-  }))
+  projectsWithTasks.push(await loadProject(p))
 }
 
 console.log(projectsWithTasks)
 
 // try to solve the problem with the query limit
 // store everything in csv
+
+console.log(projectsWithTasks.filter((p) => p.hasOwnProperty('project')).length)
--- src/getAccessToken.js
+++ src/getAccessToken.js
@@ -1,4 +1,4 @@
-import fs from 'fs'
+import fs from 'fs';
 import request from "./request.js";
 
 export default async function getAccessToken() {
